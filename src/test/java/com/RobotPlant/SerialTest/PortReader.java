feb487bf088d2a03566f7e203405c9bb4d94d244
João D. Junior <Joe_cole_23@hotmail.com>
2017-09-19 16:01:06 -0300

    Atualização de métodos
    Acréscimo de métodos para leitura dos valores seriais.

--- src/test/java/com/RobotPlant/SerialTest/PortReader.java
+++ src/test/java/com/RobotPlant/SerialTest/PortReader.java
@@ -1,10 +1,26 @@
 package com.RobotPlant.SerialTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jssc.SerialPort;
 import jssc.SerialPortEvent;
 import jssc.SerialPortEventListener;
 
 	public class PortReader implements SerialPortEventListener {
+
+		int i = 0;
+
+		boolean teste = false;
+
+		private String regra = "";
+		private String nome = "";
+
+		Double valor[] = new Double[4];
+
+		ModeloTeste modeloTeste = new ModeloTeste();
+
+		List<ModeloTeste> modeloTestes = new ArrayList<ModeloTeste>();
 
 	    private SerialPort serialPort;
 
@@ -19,7 +35,58 @@ import jssc.SerialPortEventListener;
 
 	            try {
 	                byte[] buffer = serialPort.readBytes(1);
-	                System.out.print(new String(buffer));
+
+
+	                nome += new String(buffer);
+	               // System.out.println(buffer);
+	                //System.out.print(new String(buffer));
+	                System.out.println(nome);
+
+
+	                if(nome.equals("Temperatura") || teste) {
+
+	                	if (regra.equals("Temperatura")) {
+
+
+	                		valor[i] = Double.valueOf(nome);
+
+
+
+	                		if(i>3) {
+	                			modeloTeste.setTempvalue(Double.valueOf(nome));
+	                			i=0;
+	                		}
+
+	                		i++;
+
+	                	}
+
+	                	if (nome.equals("Temperatura")) {
+
+	                		modeloTeste.setTemperatura(nome);
+	                		regra = nome;
+
+	                	}
+
+
+
+	                	nome = "";
+	                }
+
+	                if(nome.equals("\r\nnan\r\n")) {
+	                	nome = "";
+	                	teste = false;
+
+	                }
+
+	                if(nome.equals("\r\n")) {
+	                	nome = "";
+	                	teste = true;
+	                }
+
+	               // Float valor = Float.parseFloat(new String(buffer));
+	                //System.out.println(nome + " "+ valor);
+
 	            } catch (Exception e) {
 	                e.printStackTrace();
 	            }
